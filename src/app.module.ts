933fd11bdecae841f7ba98f901f36cb1b9323af9
GeovaneRigonato <geovane.rigonato.gil@gmail.com>
2023-03-17 19:59:00 -0300

    :lipstick: feat adicionando rota post cadastro de salas

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,10 @@ import { AppController } from './modules/app/app.controller';
 import { AppService } from './modules/app/app.service';
 import { PrismaService } from './database/prisma.service';
 import { UniversityModule } from './modules/university/university.module';
-import { RoomsModule } from './rooms/rooms.module';
+import { RoomModule } from './modules/room/room.module';
 
 @Module({
-  imports: [UniversityModule, RoomsModule],
+  imports: [UniversityModule, RoomModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
   exports: [PrismaService],
---
+++ src/modules/room/room.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
+import { PrismaService } from 'src/database/prisma.service';
+
+@Injectable()
+export class RoomService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(data: Prisma.RoomCreateInput) {
+    const room = await this.prisma.room.create({
+      data,
+    });
+
+    return room;
+  }
+}
