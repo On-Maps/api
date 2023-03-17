7069e94aaf60089a9b04eb72020ec25b5b0c5a3d
GeovaneRigonato <geovane.rigonato.gil@gmail.com>
2023-03-17 19:52:19 -0300

    :lipstick: feat adicionando rota post cadastro de campus

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,10 @@ import { AppController } from './modules/app/app.controller';
 import { AppService } from './modules/app/app.service';
 import { PrismaService } from './database/prisma.service';
 import { UniversityModule } from './modules/university/university.module';
-import { RoomsModule } from './rooms/rooms.module';
+import { CampusModule } from './modules/campus/campus.module';
 
 @Module({
-  imports: [UniversityModule, RoomsModule],
+  imports: [UniversityModule, CampusModule],
   controllers: [AppController],
   providers: [AppService, PrismaService],
   exports: [PrismaService],
